78a5efeacd8bf0800f3c7f2076b04bf349b90c97
Kai Aoki <kai.aoki@sigboost.audio>
2025-11-10 15:52:33 +0900

    追従

--- typings/echonet-lite/index.d.ts
+++ typings/echonet-lite/index.d.ts
@@ -126,9 +126,7 @@ declare module "echonet-lite" {
   function returner(
     bytes: Buffer,
     rinfo: rinfo,
-    userfunc:
-      | ((rinfo: rinfo, els: eldata) => void)
-      | ((rinfo: rinfo, els: eldata, e: any) => void)
+    userfunc: ((rinfo: rinfo, els: eldata, e: any) => void)
   );
   function renewFacilities(ip: string, els: eldata): void;
   function setObserveFacilities(interval: number, onChanged: () => void): void;
